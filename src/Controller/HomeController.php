c0bf1f09c9fffb4067d3ced48fdfdd846ec6eb8b
Blvck Coder <dev@blvckcoder.com>
2024-03-11 19:50:37 +0400

    FEATURE(homepage): Show list of tricks on homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\TrickRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -9,8 +10,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(TrickRepository $tricksRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('home/index.html.twig', [
+            'tricks' => $tricksRepository->findAll()
+        ]);
     }
 }
