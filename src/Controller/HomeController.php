a181e154fa625d787ec89308f4fefc8fc221dae0
Blvck Coder <dev@blvckcoder.com>
2024-03-11 19:01:49 +0400

    DESIGN(homepage): Add Bootswatch lux theme and Home twig template

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,6 +11,6 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
-        return $this->render('main/index.html.twig');
+        return $this->render('home/index.html.twig');
     }
 }
