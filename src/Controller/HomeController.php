1fe4b5e2c3fb6da087d700a709c1ba935d07d1f5
Blvck Coder <dev@blvckcoder.com>
2024-03-11 23:28:10 +0400

    FEATURE(homepage): Add pagination to list of tricks on homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\TrickRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,10 +11,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(TrickRepository $tricksRepository): Response
+    public function index(Request $request, TrickRepository $tricksRepository): Response
     {
+        $pageSize = TrickRepository::PAGINATOR_PER_PAGE;
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $tricksRepository->getTrickPaginator($offset);
+
+        $totalTricks = count($paginator);
+        $totalPages = ceil($totalTricks / $pageSize);
+
+        $previous = max($offset - $pageSize, 0);
+        $next = min($offset + $pageSize, $totalTricks - $pageSize);
+
         return $this->render('home/index.html.twig', [
-            'tricks' => $tricksRepository->findAll()
+            'tricks' => $paginator,
+            'previous' => $previous,
+            'next' => $next,
+            'totalPages' => $totalPages,
+            'currentPage' => floor($offset / $pageSize) + 1,
+            'pageSize' => $pageSize,
+            'totalTricks' => $totalTricks
         ]);
     }
 }
