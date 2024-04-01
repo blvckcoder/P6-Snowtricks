a196d0d710b03116595c1a355429b8d3e27c611e
Blvck Coder <dev@blvckcoder.com>
2024-04-01 21:59:06 +0400

    FEATURE(tricks): Show single details

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Trick;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Comment;
+use App\Form\CommentFormType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/trick', name: 'tricks_')]
 class BlogController extends AbstractController
@@ -17,8 +20,15 @@ class BlogController extends AbstractController
     }
 
     #[Route('/{slug}', name: 'show')]
-    public function show(Trick $trick): Response
+    public function show(Request $request, Trick $trick, CommentFormType $commentForm): Response
     {
-        return $this->render('tricks/show.html.twig', compact('trick'));
+        $comment = new Comment();
+        $form = $this->createForm(CommentFormType::class, $comment);
+        $form->handleRequest($request);
+        
+        return $this->render('tricks/show.html.twig', [
+            'trick' => $trick, 
+            'commentForm' => $form->createView(),
+        ]);
     }
 }
